d35aef0177e4da8caa61d1a34c74ae7cd0440701
ClecioSousa00 <cleciosouza015@gmail.com>
2023-03-21 12:40:57 -0300

    dados dinamicos adicionados

--- script/dataCity.js
+++ script/dataCity.js
@@ -2,12 +2,19 @@
 const image = document.querySelector('#js-image')
 const weather = document.querySelector('#js-weather')
 const temp = document.querySelector('#js-temp')
+const speed = document.querySelector('#js-wind-speed')
+const porcent = document.querySelector('#js-porcent')
+const cityEl = document.querySelector('#js-city')
+const country = document.querySelector('#js-country')
 
 const convertCityToHtml = async inputCity =>{
     const city = await getCityWeather(inputCity)
     image.setAttribute('src', `https://openweathermap.org/img/wn/${city.weather[0].icon}.png`)
     weather.textContent = `${city.weather[0].description}`
     temp.textContent = `${parseInt(city.main.temp)}°C`
-    
+    speed.textContent = `${city.wind.speed}km/h`
+    porcent.textContent = `${city.main.humidity}%`
+    cityEl.textContent = `${city.name}`
+    country.textContent = `${city.sys.country}`
 }
 
